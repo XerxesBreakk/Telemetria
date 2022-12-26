54e446f299393585928af495cbf54f5698b06343
Xerxesbreakk´ <carlos.alexndr@gmail.com>
2022-12-26 14:26:55 -0500

    server try

--- TSCProy/API/models.py
+++ TSCProy/API/models.py
@@ -1,4 +1,4 @@
-from datetime import datetime,timedelta
+from datetime import timedelta
 from django.db import models
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
