e03c8d47aa7021933afe6131b7a42740b07d3df4
Xerxesbreakk´ <carlos.alexndr@gmail.com>
2023-01-08 16:40:31 -0500

    Reparar refresh delta - records

--- TSCProy/API/forms.py
+++ TSCProy/API/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.forms.models import inlineformset_factory
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm,UserChangeForm
 
 class LoginForm(forms.Form):
     username = forms.CharField(label='Usuario')
@@ -6,3 +9,41 @@ class LoginForm(forms.Form):
     
     def _init_(self, *args, **kwargs):
         super(LoginForm, self)._init_(*args, **kwargs)
+        
+
+
+class UserCreationForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'first_name',
+            'last_name',
+            'email',
+            'password1',
+            'password2',
+            'is_active',
+            'is_staff',
+            'is_superuser',
+             'groups',
+        )
+        exclude =('password',
+           
+            'user_permissions' )
+        
+class UserChangeForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'first_name',
+            'last_name',
+            'email',
+            'is_staff',
+            'is_superuser',
+             'groups',
+        )
+        exclude=(
+            'password',
+        )
--- TSCProy/API/views.py
+++ TSCProy/API/views.py
@@ -5,13 +5,17 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.mixins import UserPassesTestMixin
 from django.contrib.auth.models import Group, Permission
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView,UpdateView
+from django.views.generic.edit import CreateView,UpdateView,DeletionMixin
+from django.urls import reverse_lazy
 from .models import record
 from django.contrib.auth.decorators import login_required
 from rest_framework import viewsets,permissions
-from django.contrib.auth.models import User,Group
 from .serializers import UserSerializer, GroupSerializer, RecordSerializer
 from .forms import LoginForm
+
+from django.contrib.auth.models import User,Group
+from .forms import LoginForm,UserCreationForm,UserChangeForm
+from django.contrib.auth.views import update_session_auth_hash
 
 # Create your views here.
 @login_required(login_url='/API/login/')
@@ -24,7 +28,7 @@ def lightStatus(request):
     
 
 def lightHistory(request):
-    record.refreshDelta()
+    #record.refreshDelta()
     return render(request,'lightHistory.html',{
         'records': record.objects.filter(aux=1).order_by('-id')
     })
@@ -55,8 +59,17 @@ class LoginView(FormView):
             self.alert_message = {'class': 'alert alert-danger',
             'mod': 'Error:',
             'message': u'Usuario o contraseña incorrecta.'}
+            print(self.alert_message['message'])
             return self.get(request)
         return render(request, "/")
+    
+    def get_context_data(self, **kwargs):
+        context=super(LoginView,self).get_context_data( **kwargs)
+        context['message']=self.alert_message
+        return context
+            
+        
+        
 
     
 
@@ -86,13 +99,13 @@ class LogoutView(LoginView):
 #     permission_classes = [permissions.IsAuthenticated]
     
 
-# class RecordViewSet(viewsets.ModelViewSet):
-#     """
-#     API endpoint that allows groups to be viewed or edited.
-#     """
-#     queryset = record.objects.all()
-#     serializer_class = RecordSerializer
-#     permission_classes = [permissions.IsAuthenticated]
+class RecordViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = record.objects.all()
+    serializer_class = RecordSerializer
+    permission_classes = [permissions.IsAuthenticated]
     
 class PermissionsListView(UserPassesTestMixin, ListView):
     model = Permission
@@ -112,8 +125,7 @@ class UserListView(UserPassesTestMixin, ListView):
         return self.request.user.is_superuser
     
 class UserCreate(UserPassesTestMixin, CreateView):
-    model = User
-    fields = '__all__'
+    form_class=UserCreationForm
     template_name = 'user_create.html'
     success_url = '/API/user/'
 
@@ -121,9 +133,36 @@ class UserCreate(UserPassesTestMixin, CreateView):
     def test_func(self):
         return self.request.user.is_superuser
     
+    def form_valid(self, form):
+        try:
+            context = self.get_context_data()
+            userprofile = context['userprofile']
+
+            if not User.objects.filter(username=form.instance.username).exists() and userprofile.is_valid():
+                self.object = form.save()
+                for group_ in form.cleaned_data['groups']:
+                    self.object.groups.add(group_)
+                response = super(UserCreate, self).form_valid(form)
+            else:
+                response = super(UserCreate, self).form_invalid(form)
+        except Exception as e:
+            response = super(UserCreate, self).form_invalid(form)
+        return response
+
+    
+    def form_invalid(self, form):
+        return super(UserCreate, self).form_invalid(form)
+    
+    def get_context_data(self, **kwargs):
+        context = super(UserCreate, self).get_context_data(**kwargs)
+        if self.request.POST:
+            context['userprofile'] = UserCreationForm(self.request.POST)
+        else:
+            context['userprofile'] = UserCreationForm()
+        return context
+    
 class UserUpdate(UserPassesTestMixin, UpdateView):
-    model = User
-    fields = '__all__'
+    form_class=UserChangeForm
     template_name = 'user_create.html'
     success_url = '/API/user/'
 
@@ -137,21 +176,75 @@ class UserUpdate(UserPassesTestMixin, UpdateView):
             raise Http404
         user = User.objects.get(pk=self.kwargs['id'])
         return user
+    
+    def form_valid(self, form):
+        try:
+            print("Form valid")
+            if form.is_valid():
+                print("Es valido")
+                self.object = form.save()
+                for group_ in form.cleaned_data['groups']:
+                    self.object.groups.add(group_)
+                response = super(UserUpdate, self).form_valid(form)
+            else:
+                response = super(UserUpdate, self).form_invalid(form)
+        except Exception as e:
+            response = super(UserUpdate, self).form_invalid(form)
+        return response
 
+    
+    def form_invalid(self, form):
+        print("Form Invalid")
+        return super(UserUpdate, self).form_invalid(form)
+    
     
     def get_context_data(self, **kwargs):
         context = super(UserUpdate, self).get_context_data(**kwargs)
-        context['tittle'] = "Grupos de Roles"
-        context['motto'] = "Formulario para editar un Grupo de Roles existente"
-
-        form = context['form']
-        for field in form.fields:
-            form.fields[field].widget.attrs['class'] = 'form-control'
-            if form.fields[field].help_text:
-                form.fields[field].widget.attrs['data-toggle'] = 'tooltip'
-                form.fields[field].widget.attrs['data-placement'] = 'top'
-                form.fields[field].widget.attrs['title'] = form.fields[field].help_text
+        if self.request.POST:
+            context['userprofile'] = UserChangeForm(self.request.POST)
+        else:
+            context['userprofile'] = UserChangeForm()
         return context
+    
+class UserDeleteView(UserPassesTestMixin,UpdateView):
+    model=User
+    template_name='delete_user.html'
+    success_url=reverse_lazy('UserListView')
+    fields='__all__'
+    
+    def test_func(self):
+        return self.request.user.is_superuser
+    
+    def get_object(self, queryset=None):
+        if not User.objects.filter(pk=self.kwargs['id']).exists():
+            raise Http404
+        user = User.objects.get(pk=self.kwargs['id'])
+        return user
+    
+    def form_valid(self, form):
+        try:
+            print(form.instance.id)
+            print(form.cleaned_data['id'])
+            print(form.cleaned_data['username'])
+            user = User.objects.get(pk=form.instance.id)
+            user.is_active = False
+            user.save()
+            response = super(UserUpdate, self).form_valid(form)
+        except Exception as e:
+            response = super(UserUpdate, self).form_invalid(form)
+        return response
+    
+    def post(self,request,id):
+        try:
+            user=User.objects.get(pk=id)
+            user.is_active=False
+            user.save()
+            print("Usuario eliminado")
+            return redirect('UserListView')
+            
+        except Exception as e:
+            return redirect('/')
+    
     
     
     
@@ -177,7 +270,7 @@ class GroupUpdate(UserPassesTestMixin, UpdateView):
     model = Group
     fields = '__all__'
     template_name = 'group_create.html'
-    success_url = '/API/user/'
+    success_url = '/API/group/'
 
     
     def test_func(self):
